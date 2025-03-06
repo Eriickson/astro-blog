6eab972fc5e2d32626a7bc43f07f6fe8362e1eac
Erickson Manuel Holguín <erickson.pena@kiura.co>
2025-03-06 07:17:52 -0400

    Enhance BlogPost component with dynamic data handling and date formatting; update index page to render posts dynamically

---
+++ src/utils/formatter.ts
@@ -0,0 +1,9 @@
+export class Formatter {
+  static formatDate(date: Date) {
+    return new Intl.DateTimeFormat('en-US', {
+      year: 'numeric',
+      month: 'long',
+      day: '2-digit',
+    }).format(new Date(date));
+  }
+}
