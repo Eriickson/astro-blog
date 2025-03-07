07820c6a18e34fc1f653b27ac287a3dfa865c4ca
Erickson Manuel Holguín <erickson.pena@kiura.co>
2025-03-06 20:17:05 -0400

    Refactor blog post images to use new Image component for better performance; add new blog images and update image paths in posts

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,11 +2,12 @@ import { defineCollection, z } from "astro:content";
 
 const blogCollection = defineCollection({
   type: "content",
-  schema: z.object({
+  schema: ({image}) => z.object({
     title: z.string(),
     date: z.date(),
     description: z.string(),
-    image: z.string(),
+    image: image(),
+    // image: z.string(),
 
     // Relation
     author: z.string(),
