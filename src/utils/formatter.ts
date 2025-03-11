0b67c3e954d30c2d5e0b8b52363ad9c60e511a50
Erickson Manuel Holguín <erickson.pena@kiura.co>
2025-03-11 07:53:24 -0400

    Refactor code style for consistency; update image paths and formatting in various files

--- src/utils/formatter.ts
+++ src/utils/formatter.ts
@@ -1,9 +1,9 @@
 export class Formatter {
   static formatDate(date: Date) {
-    return new Intl.DateTimeFormat('en-US', {
-      year: 'numeric',
-      month: 'long',
-      day: '2-digit',
+    return new Intl.DateTimeFormat("en-US", {
+      year: "numeric",
+      month: "long",
+      day: "2-digit",
     }).format(new Date(date));
   }
 }
