6f99d226c1538772150b585b85770099b9caff8c
Erickson Manuel Holguín <erickson.pena@kiura.co>
2025-03-11 07:38:20 -0400

    Add Prettier configuration and author metadata; update blog post authors and enhance author display in posts

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,22 +1,34 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection, reference, z } from "astro:content";
 
 const blogCollection = defineCollection({
   type: "content",
-  schema: ({image}) => z.object({
-    title: z.string(),
-    date: z.date(),
-    description: z.string(),
-    image: image(),
-    // image: z.string(),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      date: z.date(),
+      description: z.string(),
+      image: image(),
+      // image: z.string(),
 
-    // Relation
-    author: z.string(),
+      // Relation
+      // author: z.string(),
+      author: reference("author"),
 
-    // Relation
-    tags: z.array(z.string()),
-  })
-})
+      // Relation
+      tags: z.array(z.string()),
+    }),
+});
+
+const authorCollection = defineCollection({
+  type: "data",
+  schema: ({ image }) =>
+    z.object({
+      name: z.string(),
+      avatar: image(),
+    }),
+});
 
 export const collections = {
   blog: blogCollection,
-}
+  author: authorCollection,
+};
